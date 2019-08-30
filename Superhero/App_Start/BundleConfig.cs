65716473048906632cd64656716a24e3926c3edc
Jon Ransom <jransom12@live.com>
2019-08-30 13:28:15 -0500

    Bootstrap and superhero image added

--- Superhero/App_Start/BundleConfig.cs
+++ Superhero/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace SuperheroProject
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/superherobootstrap.css",
+                      "~/Content/Site.css"));
         }
     }
 }
